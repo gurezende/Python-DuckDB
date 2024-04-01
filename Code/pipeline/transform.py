4143ca493e958a603ef25db04bce5e3dacd7beba
gurezende <gurezendes@gmail.com>
2024-04-01 12:30:36 -0400

    Update code docstring

--- Code/pipeline/transform.py
+++ Code/pipeline/transform.py
@@ -1,11 +1,14 @@
 import pandas as pd
 from typing import List
 
-'''
-Function to transform a list of dataframes in a single dataframe
-'''
+
 
 def concat_dfs(dtf_list: List[pd.DataFrame]) -> pd.DataFrame:
+
+    '''
+    Function to transform a list of dataframes in a single dataframe
+    '''
+
     return pd.concat(dtf_list, ignore_index=True)
 
 
